42cdacb427ded5d161284e2bea27834f348f215c
Felix Arntz <felix-arntz@t-online.de>
2016-07-05 17:48:41 +0200

    added some documentation

--- core/models/class-field-mapper.php
+++ core/models/class-field-mapper.php
@@ -5,7 +5,7 @@
  * @package TorroForms
  * @subpackage CoreModels
  * @version 1.0.0-beta.6
- * @since 1.0.0-beta.1
+ * @since 1.1.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -13,16 +13,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Torro Forms Main Extension Class
+ * Torro Forms Field Mapper Class
  *
- * This class is the base for every Torro Forms Extension.
+ * This class allows to define mappings between a set of fields and elements of a specific form.
+ * The class takes care of the functionality while the Field Mappers Manager handles the UI.
  *
- * @since 1.0.0-beta.1
+ * @since 1.1.0
  */
 abstract class Torro_Field_Mapper extends Torro_Base {
 
 	/**
 	 * The fields this mapper requires.
+	 *
+	 * These should be declared in the `init` method.
 	 *
 	 * @since 1.1.0
 	 * @var array
@@ -30,9 +33,9 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 	protected $fields = array();
 
 	/**
-	 * Initializing.
+	 * Initializes the field mapper.
 	 *
-	 * @since 1.0.0
+	 * @since 1.1.0
 	 */
 	protected function __construct() {
 		parent::__construct();
@@ -40,10 +43,25 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		add_filter( 'torro_response_status', array( $this, 'check_response' ), 10, 5 );
 	}
 
+	/**
+	 * Returns the fields for this mapper.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @return array
+	 */
 	public function get_fields() {
 		return $this->fields;
 	}
 
+	/**
+	 * Returns the mappings of a specific form for this mapper.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $form_id
+	 * @return array|Torro_Error
+	 */
 	public function get_mappings( $form_id ) {
 		$form = torro()->forms()->get( $form_id );
 		if ( is_wp_error( $form ) ) {
@@ -78,6 +96,14 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		return $validated_mappings;
 	}
 
+	/**
+	 * Returns the mapped values of a specific form submission for this mapper.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param int $result_id
+	 * @return array|Torro_Error
+	 */
 	public function get_mapped_values( $result_id ) {
 		$result = torro()->results()->get( $result_id );
 		if ( is_wp_error( $result ) ) {
@@ -114,6 +140,20 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		return $mappings;
 	}
 
+	/**
+	 * Checks the response (on form submission).
+	 *
+	 * This ensures that all fields required for the mapper were filled properly.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param bool $status
+	 * @param int $form_id
+	 * @param int $container_id
+	 * @param bool $is_submit
+	 * @param Torro_Form_Controller_Cache $cache
+	 * @return bool
+	 */
 	public function check_response( $status, $form_id, $container_id, $is_submit, $cache ) {
 		if ( ! $is_submit ) {
 			return $status;
@@ -132,10 +172,26 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		return $status;
 	}
 
+	/**
+	 * Validates a response with this mapper.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param array $response
+	 * @param int $form_id
+	 * @return bool|Torro_Error
+	 */
 	public function validate_response( $response, $form_id ) {
 		//TODO
 	}
 
+	/**
+	 * Validates the fields of this mapper.
+	 *
+	 * This method is invoked in the constructor.
+	 *
+	 * @since 1.1.0
+	 */
 	protected function validate_fields() {
 		$defaults = array(
 			'type'				=> 'string',
@@ -159,6 +215,16 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		}
 	}
 
+	/**
+	 * Validates a result value.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param mixed $value
+	 * @param mixed $choices
+	 * @param string $type
+	 * @return mixed|Torro_Error
+	 */
 	protected function validate_from_choices( $value, $choices, $type ) {
 		switch ( $type ) {
 			case 'float':
@@ -209,6 +275,17 @@ abstract class Torro_Field_Mapper extends Torro_Base {
 		return $value;
 	}
 
+	/**
+	 * Returns the default value of a field of this mapper.
+	 *
+	 * This method is used if no default is provided.
+	 *
+	 * @since 1.1.0
+	 *
+	 * @param mixed $choices
+	 * @param string $type
+	 * @return mixed
+	 */
 	protected function get_default_from_choices( $choices, $type ) {
 		if ( is_array( $choices ) && 0 < count( $choices ) ) {
 			if ( isset( $choices[0] ) ) {
